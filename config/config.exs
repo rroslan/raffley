c7f58fe4d16806ae7040e126e6a5c1db29871a1c
rroslan <rroslan@gmail.com>
2025-04-27 07:42:19 +0800

    add auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :raffley, :scopes,
+  user: [
+    default: true,
+    module: Raffley.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Raffley.AccountsFixtures,
+    test_login_helper: :register_and_log_in_user
+  ]
+
 config :raffley,
   ecto_repos: [Raffley.Repo],
   generators: [timestamp_type: :utc_datetime]
