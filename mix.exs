82dc9184fb0a0ecf532307ff0466b7993c3c3be0
rroslan <rroslan@gmail.com>
2025-04-26 09:03:33 +0800

    update resend version

--- mix.exs
+++ mix.exs
@@ -59,7 +59,7 @@ defmodule Raffley.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:resend, "~> 0.4.0"}
+      {:resend, "~> 0.4.4"}
     ]
   end
 
