86b3badc480aec03c488d083a558c537cff820ff
rroslan <rroslan@gmail.com>
2025-05-03 20:33:32 +0800

    style: improve header layout with logo as home link
    - Make logo more prominent (42px with min-width)
    - Clean up navigation to show only logo and theme toggle
    - Ensure logo works as home link with hover effect
    - Maintain responsive padding and spacing

--- lib/raffley_web/components/layouts.ex
+++ lib/raffley_web/components/layouts.ex
@@ -16,24 +16,13 @@ defmodule RaffleyWeb.Layouts do
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
         <.link href="/" class="flex items-center gap-2 hover:opacity-75 transition-opacity">
-          <img src={~p"/images/logo.svg"} width="36" alt="Raffley Logo" />
+          <img src={~p"/images/logo.svg"} width="42" class="min-w-[42px]" alt="Raffley Logo" />
         </.link>
       </div>
       <div class="flex-none">
         <ul class="flex flex-column px-1 space-x-4 items-center">
           <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
             <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
           </li>
         </ul>
       </div>
